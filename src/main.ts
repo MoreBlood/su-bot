b6d277e57f5f7b939d7deac071df36b7f61177b5
Kirill Artihovich <new.shtirlic@mail.ru>
2022-06-10 00:14:08 +0300

    ~ some fixes and refactor

--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,30 @@
 import { config } from 'dotenv';
-// import { REST } from '@discordjs/rest';
-import { Client, Intents } from 'discord.js';
-import { Routes } from 'discord-api-types/v9';
+import { Client } from 'discord.js';
 import TelegramBot from 'node-telegram-bot-api';
 import http from 'http';
 
 config({ path: `.env.local` });
 
 const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
-
-// let chatId: number | undefined;
-
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
-
-//   chatId = msg.chat.id;
-//   console.log(chatId);
-//   const resp = match[1]; // the captured "whatever"
-
-//   // // send back the matched "whatever" to the chat
-//   // bot.sendMessage(chatId, resp);
-// });
-
-// const commands = [
-//   {
-//     name: 'ping',
-//     description: 'Replies with Pong!',
-//   },
-// ];
-
-// const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-
-// (async () => {
-//   try {
-//     console.log('Started refreshing application (/) commands.');
-
-//     await rest.put(
-//       Routes.applicationGuildCommands(
-//         process.env.CLIENT_ID,
-//         process.env.GUILD_ID,
-//       ),
-//       { body: commands },
-//     );
-
-//     console.log('Successfully reloaded application (/) commands.');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
 const client = new Client();
+const server = http.createServer(requestListener);
+
+client.login(process.env.TOKEN);
+
+function sendMessageToChat(message: string) {
+  bot.sendMessage(process.env.TG_CHAT_ID, message, {
+    disable_notification: true,
+  });
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
 client.on('voiceStateUpdate', async (before, after) => {
-  // const membersBefore = before.channel?.members;
-  // const membersAfter = after.channel?.members;
-
-  if (after.channelID !== process.env.DISCORD_VOICE_CHAT_ID) {
+  if (
+    after.channelID.toString() !== process.env.DISCORD_VOICE_CHAT_ID.toString()
+  ) {
     return;
   }
 
@@ -77,25 +32,17 @@ client.on('voiceStateUpdate', async (before, after) => {
   // TODO: clean messages after some time
 
   if (after.channel?.members.size === 1) {
-    bot.sendMessage(
-      process.env.TG_CHAT_ID,
+    sendMessageToChat(
       [`${after.member.displayName} joined ${after.channel.name}`].join('\n'),
-      {
-        disable_notification: true,
-      },
     );
   }
 
   if (before.channel?.members.size === 0 && after.channelID === null) {
-    bot.sendMessage(
-      process.env.TG_CHAT_ID,
+    sendMessageToChat(
       [
         `${after.member.displayName} left ${before.channel.name}`,
         `no users left`,
       ].join('\n'),
-      {
-        disable_notification: true,
-      },
     );
   }
 
@@ -108,37 +55,18 @@ client.on('voiceStateUpdate', async (before, after) => {
       .map((act) => act.name)
       .join(', ');
 
-    bot.sendMessage(
-      process.env.TG_CHAT_ID,
+    sendMessageToChat(
       [
         `${after.member.displayName} ${
           after.streaming ? 'Started streaming 🔴' : 'Ended streaming 😴'
         } `,
         after.streaming ? `🎮 ${game || 'Unknown game'}` : '',
       ].join('\n'),
-      {
-        disable_notification: true,
-      },
     );
   }
-
-  // console.log(
-  //   'before',
-  //   before.member?.displayName,
-  //   before.channel?.name,
-  //   membersBefore?.map((member) => member.displayName),
-  // );
-  // console.log(
-  //   'after',
-  //   before.member?.displayName,
-  //   before.channel?.name,
-  //   membersAfter?.map((member) => member.displayName),
-  // );
 });
 
-client.login(process.env.TOKEN);
-
-const requestListener = function (request, response) {
+function requestListener(request, response) {
   response.writeHead(200, {
     'Content-Type': 'text/plain',
     'Content-Length': 2,
@@ -146,7 +74,6 @@ const requestListener = function (request, response) {
 
   response.write('OK');
   response.end();
-};
+}
 
-const server = http.createServer(requestListener);
 server.listen(8080);
