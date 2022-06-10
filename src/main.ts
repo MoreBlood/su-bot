4dc74758b7da385f244b05ad21db8a4767c41297
Kirill Artihovich <new.shtirlic@mail.ru>
2022-06-10 23:02:46 +0300

    ~ refactoring

--- src/main.ts
+++ src/main.ts
@@ -5,66 +5,11 @@ import http from 'http';
 
 config({ path: `.env.local` });
 
-const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
-const client = new Client();
-const server = http.createServer(requestListener);
-
-client.login(process.env.TOKEN);
-
 function sendMessageToChat(message: string) {
   bot.sendMessage(process.env.TG_CHAT_ID, message, {
     disable_notification: true,
   });
 }
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('voiceStateUpdate', async (before, after) => {
-  if (
-    after.channelID.toString() !== process.env.DISCORD_VOICE_CHAT_ID.toString()
-  ) {
-    return;
-  }
-
-  // TODO: if user start playing World Of Tanks, and plays for 5 minutes then send notification (not afk)
-  // TODO: clean messages after some time
-
-  if (after.channel?.members.size === 1) {
-    sendMessageToChat(
-      [`${after.member.displayName} joined ${after.channel.name}`].join('\n'),
-    );
-  }
-
-  if (before.channel?.members.size === 0 && after.channelID === null) {
-    sendMessageToChat(
-      [
-        `${after.member.displayName} left ${before.channel.name}`,
-        `no users left`,
-      ].join('\n'),
-    );
-  }
-
-  if (
-    after.id === before.id &&
-    after.streaming !== before.streaming &&
-    (before.channelID === after.channelID || after.channelID === null)
-  ) {
-    const game = after.member.presence?.activities
-      .map((act) => act.name)
-      .join(', ');
-
-    sendMessageToChat(
-      [
-        `${after.member.displayName} ${
-          after.streaming ? 'Started streaming 🔴' : 'Ended streaming 😴'
-        } `,
-        after.streaming ? `🎮 ${game || 'Unknown game'}` : '',
-      ].join('\n'),
-    );
-  }
-});
 
 function requestListener(request, response) {
   response.writeHead(200, {
@@ -76,4 +21,78 @@ function requestListener(request, response) {
   response.end();
 }
 
+// TELEGRAM
+const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
+
+// DISCORD
+const client = new Client();
+client.login(process.env.TOKEN);
+
+// HTTP HELTH CHECK
+const server = http.createServer(requestListener);
 server.listen(8080);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// TODO: if user start playing World Of Tanks, and plays for 5 minutes then send notification (not afk)
+// TODO: clean messages after some time
+
+client.on('voiceStateUpdate', async (before, after) => {
+  const isTargetChannelAfter =
+    after.channelID.toString() === process.env.DISCORD_VOICE_CHAT_ID.toString();
+  const isTargetChannelBefore =
+    before.channelID.toString() ===
+    process.env.DISCORD_VOICE_CHAT_ID.toString();
+
+  const isTheSameUser = after.id === before.id;
+  const streamingChanged = after.streaming !== before.streaming;
+
+  const isTheSameChannel = before.channelID === after.channelID;
+  const hasNoChannel = after.channelID === null;
+
+  const hasOneJoinedUser = after.channel?.members.size === 1;
+  const hasNoUsersLeft = before.channel?.members.size === 0;
+
+  // triggers when channel has no left users after user left
+  if (hasNoUsersLeft && hasNoChannel && isTargetChannelBefore) {
+    sendMessageToChat(
+      [
+        `${after.member.displayName} left ${before.channel.name}`,
+        `no users left`,
+      ].join('\n'),
+    );
+  }
+
+  // triggers only users joins empty channel
+  if (hasOneJoinedUser && isTargetChannelAfter) {
+    sendMessageToChat(
+      [`${after.member.displayName} joined ${after.channel.name}`].join('\n'),
+    );
+  }
+
+  const userStreamingStatusChanged = isTheSameUser && streamingChanged;
+  const streamingHappeningInTheSameChannel =
+    isTheSameChannel && isTargetChannelAfter;
+  const streamingStopsInTargetChannel = hasNoChannel && isTargetChannelBefore;
+
+  // triggers when user streaming activity changes
+  if (
+    userStreamingStatusChanged &&
+    (streamingHappeningInTheSameChannel || streamingStopsInTargetChannel)
+  ) {
+    const game = after.member.presence?.activities
+      .map((act) => act.name)
+      .join(', ');
+
+    const streamStatus = after.streaming
+      ? 'Started streaming 🔴'
+      : 'Ended streaming 😴';
+    const gameStatus = after.streaming ? `🎮 ${game || 'Unknown game'}` : '';
+
+    sendMessageToChat(
+      [`${after.member.displayName} ${streamStatus} `, gameStatus].join('\n'),
+    );
+  }
+});
